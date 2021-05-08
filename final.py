d1d714d89f2fc2c60f4fc06f625822de697bb8e1
Maaz <maaz93200@gmail.com>
2021-05-08 17:52:21 +0200

    Version 1.2

--- final.py
+++ final.py
@@ -31,22 +31,42 @@ def common_output(pdb_file, dssp_file, dssp2_file, stride_file,sst_file):
 
     #liste de position des flags
     flags=[]
-    Remarque=""
-    #compare dssp et stride
+    Remarque1=""
+    Remarque2=""
+    Remarque3=""
+    Remarque4=""
+    #compare dssp et stride acides amine
     compare_dssp_stride=compare_seq(aa_pos,aa_seq,aa_pos_stride, aa_seq_stride)
-    print(compare_dssp_stride)
+
     if(len(compare_dssp_stride)>0):
         flags+=compare_dssp_stride
 
-    #compare dssp et promotif
+    #compare dssp et promotif acides amine
     compare_dssp_promotif=compare_seq(aa_pos,aa_seq,aa_pos_promotif, aa_seq_promotif)
-
     if(len(compare_dssp_promotif)>0):
         flags+=compare_dssp_promotif
 
-    print(flags)
+    #compare dssp et stride structure secondaire
+    compare_dssp_stride_struct=compare_seq(aa_pos,dssp_struct_seq,aa_pos_stride, stride_struct_seq)
+    if(len(compare_dssp_stride_struct)>0):
+        flags+=compare_dssp_stride_struct
+
+    #compare dssp et stride structure secondaire
+    compare_dssp_promotif_struct=compare_seq(aa_pos,dssp_struct_seq,aa_pos_promotif, promotif_struct_seq)
+    if(len(compare_dssp_promotif_struct)>0):
+        flags+=compare_dssp_promotif_struct
+
+
+    #matrice de confusion dssp promotif
+    matrice_dssp_promotif = matrice_seq_8etats(dssp_struct_seq,promotif_struct_seq)
+    affichage_matrice_dssp_promotif = "              Matrice de confusuion DSSP et Prmotif\n"+print_matrix_8etats(matrice_dssp_promotif)
+
+    #matrice de confusion dssp promotif
+    matrice_dssp_stride = matrice_seq_7etats(dssp_struct_seq,stride_struct_seq)
+    affichage_matrice_dssp_stride = "              Matrice de confusuion DSSP et Stride\n"+print_matrix_7etats(matrice_dssp_stride)
+
     flags=[int(i) for i in flags]
-    print(flags)
+
     with open(common_file_parsed, "w+") as file_out:
         file_out.write("  POS C A D 2 S P\n")
         x=0
@@ -59,16 +79,33 @@ def common_output(pdb_file, dssp_file, dssp2_file, stride_file,sst_file):
                 x+=1
             else:
                 if k in flags:
-                    print("1")
+                    
                     file_out.write(" {} {} {} {} {} {} {} {}\n".format(aa_pos[k], chain_ID_seq[k], aa_seq[k], dssp_struct_seq[k],
                                                          dssp2_struct_seq[k], stride_struct_seq[k], promotif_struct_seq[k],"#"))
-                if k in compare_dssp_stride :
-                    Remarque="#1 pos:{} chain:{} DSSP:{}  Stride:{}\n ".format(aa_pos[k],chain_ID_seq[k],aa_seq[k],aa_seq_stride[k])
-                if k in compare_dssp_promotif:
-                    Remarque="#2 pos:{} chain:{} DSSP:{}  Promotif:{}\n ".format(aa_pos[k],chain_ID_seq[k],aa_seq[k],aa_seq_promotif[k])
-                file_out.write(" {} {} {} {} {} {} {}\n".format(aa_pos[k], chain_ID_seq[k], aa_seq[k], dssp_struct_seq[k],
+                    if k in compare_dssp_stride :
+                        Remarque1=Remarque1+"Remark #1 pos:{} chain:{} DSSP:{}  Stride:{}\n".format(aa_pos[k],chain_ID_seq[k],aa_seq[k],aa_seq_stride[k])
+
+                    if k in compare_dssp_promotif:
+                        Remarque2=Remarque2+"Remark #2 pos:{} chain:{} DSSP:{}  Promotif:{}\n".format(aa_pos[k],chain_ID_seq[k],aa_seq[k],aa_seq_promotif[k])
+
+                    if k in compare_dssp_stride_struct :
+                        Remarque3=Remarque3+"Remark #3 pos:{} chain:{} DSSP:{}  Stride:{}\n".format(aa_pos[k],chain_ID_seq[k],dssp_struct_seq[k],stride_struct_seq[k])
+
+                    if k in compare_dssp_promotif_struct :
+                        Remarque4=Remarque4+"Remark #4 pos:{} chain:{} DSSP:{}  Promotif:{}\n".format(aa_pos[k],chain_ID_seq[k],dssp_struct_seq[k],promotif_struct_seq[k])
+                
+                else:
+                    file_out.write(" {} {} {} {} {} {} {}\n".format(aa_pos[k], chain_ID_seq[k], aa_seq[k], dssp_struct_seq[k],
                                                          dssp2_struct_seq[k], stride_struct_seq[k], promotif_struct_seq[k]))
-        file_out.write(Remarque)
+        
+        file_out.write("\n"+affichage_matrice_dssp_stride+"\n")
+        file_out.write(affichage_matrice_dssp_promotif+"\n")
+        file_out.write(Remarque1+"\n")
+        file_out.write(Remarque2+"\n")
+        file_out.write(Remarque3+"\n")
+        file_out.write(Remarque4+"\n")
+
+
 
 
 
--- missing_residues.py
+++ missing_residues.py
@@ -25,12 +25,98 @@ def compare_seq( pos_base, sequence_base, pos_test,sequence_test):
 				pos_difference.append(i)
 	return pos_difference 
 
+
+def matrice_seq_8etats(sequence_dssp,sequence_promotif):
+	DSSP={'H' : 0,'G' : 1,'I' : 2,'B' : 3,'E' : 4,'T' : 5,'C' : 6,'S' : 7}
+	Promotif={'H' : 0,'G' : 1,'I' : 2,'B' : 3,'E' : 4,'T' : 5,'C' : 6,'S' : 7}
+	matrix=[]
+	for i in range(8):
+		matrix.append([])
+		for j in range(8):
+			matrix[i].append(0)
+
+	for i in range(len(sequence_dssp)):
+		a=DSSP[sequence_dssp[i]]
+		b=Promotif[sequence_promotif[i]]
+		matrix[a][b]+=1
+		
+	return matrix
+
+def matrice_seq_7etats(sequence_dssp,sequence_stride):
+	DSSP={'H' : 0,'G' : 1,'I' : 2,'B' : 3,'E' : 4,'T' : 5,'C' : 6,'S' : 7}
+	Stride={'H' : 0,'G' : 1,'I' : 2,'B' : 3,'E' : 4,'T' : 5,'C' : 6}
+	matrix=[]
+	for i in range(8):
+		matrix.append([])
+		for j in range(8):
+			matrix[i].append(0)
+
+	for i in range(len(sequence_dssp)):
+		a=DSSP[sequence_dssp[i]]
+		b=Stride[sequence_stride[i]]
+		matrix[a][b]+=1
+		
+	return matrix
+
+def print_matrix_8etats(matrix):
+	matrix = [[str(ele) for ele in sub] for sub in matrix]
+	DSSP={0 : 'H' ,1:'G' , 2:'I' , 3:'B' , 4:'E' , 5:'T' , 6:'C' ,7:'S' }
+
+	S="                            Promotif\n"
+	S+="         H      G      I      B      E      T      C      S\n"
+
+	for i in range(8):
+		for j in range(8):
+			if len(matrix[i][j]) == 1:
+				matrix[i][j] = "   "+matrix[i][j]
+			elif len(matrix[i][j]) == 2:
+				matrix[i][j] = "  "+matrix[i][j]
+			elif len(matrix[i][j]) == 3:
+				matrix[i][j] = " "+matrix[i][j]
+
+
+	for i in range(8):
+		if i == 4:
+			S+="DSSP {} {} | {} | {} | {} | {} | {} | {} | {}\n".format(DSSP[i],matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],
+				matrix[i][4],matrix[i][5],matrix[i][6],matrix[i][7])
+		else:
+			S+="     {} {} | {} | {} | {} | {} | {} | {} | {}\n".format(DSSP[i],matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],
+				matrix[i][4],matrix[i][5],matrix[i][6],matrix[i][7])
+		S+="       -----------------------------------------------------\n"
+	return S
+
+def print_matrix_7etats(matrix):
+	matrix = [[str(ele) for ele in sub] for sub in matrix]
+	DSSP={0 : 'H' ,1:'G' , 2:'I' , 3:'B' , 4:'E' , 5:'T' , 6:'C' ,7:'S' }
+
+	S="                            Stride\n"
+	S+="         H      G      I      B      E      T      C     \n"
+
+	for i in range(8):
+		for j in range(7):
+			if len(matrix[i][j]) == 1:
+				matrix[i][j] = "   "+matrix[i][j]
+			elif len(matrix[i][j]) == 2:
+				matrix[i][j] = "  "+matrix[i][j]
+			elif len(matrix[i][j]) == 3:
+				matrix[i][j] = " "+matrix[i][j]
+
+
+	for i in range(8):
+		if i == 4:
+			S+="DSSP {} {} | {} | {} | {} | {} | {} | {} \n".format(DSSP[i],matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],
+				matrix[i][4],matrix[i][5],matrix[i][6])
+		else:
+			S+="     {} {} | {} | {} | {} | {} | {} | {} \n".format(DSSP[i],matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],
+				matrix[i][4],matrix[i][5],matrix[i][6])
+		S+="       ------------------------------------------------\n"
+	return S
 """def main():
-    pos_base=['1','2','3','4']
-    sequence_base='BBCD'
-    pos_test=['1','2','3','4']
-    sequence_test='ABCD'
-    print(compare_seq(pos_base, sequence_base, pos_test,sequence_test))
+	pos_base=['1','2','3','4']
+	sequence_base='HCHH'
+	pos_test=['1','2','3','4']
+	sequence_test='HHHH'
+	print(matrice_seq_8etats(sequence_base,sequence_test))
 
 
 main()"""
